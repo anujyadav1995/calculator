69d1f41a408492750823896c48df808afc476832
anuj <anuj@Anujs-MacBook-Air.local>
2024-12-22 20:36:34 +0530

    test case for single number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,10 +2,11 @@ require 'string_calculator'
 
 RSpec.describe StringCalculator do
     describe '.add' do
-        context 'when the input return empty' do
-            it 'return 0' do
-                expect(StringCalculator.add("")).to eq(0)
-            end
-        end 
+        it 'return 0' do
+            expect(StringCalculator.add("")).to eq(0)
+        end
+        it 'when there is a single number' do
+            expect(StringCalculator.add("1")).to eq(1)
+        end
     end
 end
